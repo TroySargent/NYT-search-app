ec8185b1b926c002affd421bd068b4f5612d3121
Aaron Tobey <abtobey@gmail.com>
2020-07-31 13:04:25 -0500

    Input boxes

--- index.js
+++
@@ -1,9 +0,0 @@
-let apiKey = "Wd4pxwZ6afgbEH60WOkHZde44gsP6khq"
-let queryUrl = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTerm}&api-key=${apiKey}`
-
-$.ajax({
-    url : queryUrl,
-    method : "GET",
-}).then(function(response){
-
-});
